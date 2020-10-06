2a57e301a206f9b8ade53871c63ebe6a51f67c14
nugraha <nugraha@192.168.0.8>
2020-10-06 13:12:36 +0700

    loop

--- src/main/java/com/google/adit/camel/MySpringBootRouter.java
+++ src/main/java/com/google/adit/camel/MySpringBootRouter.java
@@ -2,7 +2,11 @@ package com.google.adit.camel;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.redis.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
@@ -12,13 +16,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySpringBootRouter extends RouteBuilder {
 
-    @Override
+	@Override
     public void configure() {
     	rest("/redis/")
-	        .get("/{key}").to("direct:get")
-	        .get("/{key}/{value}").to("direct:set");
-    	
-    	from("timer:hello?period={{timer.period}}").routeId("hello")
+	        .get("/single/{key}").to("direct:get")
+	        .get("/single/{key}/{value}").to("direct:set")
+	    	.get("/loop/{loop}/{key}/{value}").to("direct:loop");
+	
+    	from("timer:hello?period={{timer.period}}").routeId("hello").autoStartup(false)
             .log("key-${exchangeProperty.CamelTimerFiredTime.getTime}")
             .setHeader("key").simple("key-${exchangeProperty.CamelTimerFiredTime.getTime}")
 		    .setHeader("value").simple("value-${exchangeProperty.CamelTimerFiredTime.getTime}")
@@ -26,16 +31,26 @@ public class MySpringBootRouter extends RouteBuilder {
             .to("direct:get")
 		    .log("${body}");
     	
+    	from("direct:loop")
+    		.setProperty("key").simple("${header.key}")
+    		.setProperty("value").simple("${header.value}")
+    		.loop(simple("${header.loop}"))
+    			.setProperty("timestamp").simple("${date:now:yyyyMMddHHmmssSSS}")
+    			.setHeader("key").simple("${exchangeProperty.key}-${exchangeProperty.timestamp}")
+    			.setHeader("value").simple("${exchangeProperty.value}-${exchangeProperty.timestamp}")
+    			.to("direct:set");
+	
+    	
     	from("direct:get")
 	    	.setHeader(RedisConstants.COMMAND).simple("GET")
 		    .setHeader(RedisConstants.KEY).simple("${header.key}")
-		    .to("spring-redis://{{redis.host}}?serializer=StringRedisSerializer");
+		    .to("spring-redis://{{redis.host}}");
     	
     	from("direct:set")
 	    	.setHeader(RedisConstants.COMMAND).simple("SET")
 		    .setHeader(RedisConstants.KEY).simple("${header.key}")
 		    .setHeader(RedisConstants.VALUE).simple("${header.value}")
-            .to("spring-redis://{{redis.host}}?serializer=StringRedisSerializer");
+            .to("spring-redis://{{redis.host}}");
 	
         
 //        from("spring-redis://localhost:6379?command=SUBSCRIBE&channels=testChannel")
